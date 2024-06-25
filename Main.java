3f814c9c968d352dd4ebb5aca2fd92b0de18dcda
Олександр Сенишин <rakzar041@gmail.com>
2024-06-25 20:20:07 +0300

    Update Main.java
    Home work 2

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ");
+         int Apples = 10;
+        byte tortugas = 7;
+        short boys = 3;
+        long dolars = 100000L;
+        float i = 3.2e-2f;
+        double P = 3.14;
+        char absolut = 34;
+        System.out.println("Apples tortugas boys dolars i P absolut");
+        System.out.println(Apples);
+        System.out.println(tortugas);
+        System.out.println(boys);
+        System.out.println(dolars);
+        System.out.println(i);
+        System.out.println(P);
+        System.out.println(absolut);
     }
 }
