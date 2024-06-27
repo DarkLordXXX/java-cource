4044c49a2dcd6975819c57157aa4d9e40410c013
Your Name <rakzar041@gmail.com>
2024-06-27 21:48:27 +0300

    Перейменував змінні.

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ");
-         int Apples = 10;
+        int apples = 10;
         byte tortugas = 7;
         short boys = 3;
-        long dolars = 100000L;
+        long dollars = 100000L;
         float i = 3.2e-2f;
         double P = 3.14;
         char absolut = 34;
         System.out.println("Apples tortugas boys dolars i P absolut");
-        System.out.println(Apples);
+        System.out.println(apples);
         System.out.println(tortugas);
         System.out.println(boys);
-        System.out.println(dolars);
+        System.out.println(dollars);
         System.out.println(i);
         System.out.println(P);
         System.out.println(absolut);
