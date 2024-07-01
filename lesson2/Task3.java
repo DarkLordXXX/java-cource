f567a5fc9e8f4794d8059c4e9c3aa3142aa908c3
Your Name <rakzar041@gmail.com>
2024-07-01 23:28:29 +0300

    Добавив завдання про клас сканер але він не працює і я не розумію чому

---
+++ lesson2/Task3.java
@@ -0,0 +1,19 @@
+package lesson2;     java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int i1 = scanner.nextInt();
+        // оголошуємо змінну i2 типу int і присвоюємо їй значення числа,
+        // зчитаного з клавіатури за допомогою методу nextInt
+        int i2 = scanner.nextInt();
+        // оголошуємо змінну i3 типу int і присвоюємо їй значення числа,
+        // зчитаного з клавіатури за допомогою методу nextInt
+        int i3 = scanner.nextInt();
+        System.out.println((i1 + i2 + i3) / 3);
+        // Пробував використати метод сканер не працює
+        //'class' or 'interface' expected
+        //Cannot resolve symbol 'scanner'
+        //Cannot resolve symbol 'scanner'
+        //Cannot resolve symbol 'scanner'
+    }
+}
