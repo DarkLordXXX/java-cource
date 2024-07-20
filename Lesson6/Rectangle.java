698ad2f56e7bf7c713dec3add3efa392ce6a084e
Your Name <rakzar041@gmail.com>
2024-07-20 22:04:15 +0300

    Тут дз 6 уроку 1 завдання Обчислення площі прямокутника.

---
+++ Lesson6/Rectangle.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+        int area = a * b;
+        System.out.println("Периметр прямокутника " + area);
+    }
+}
+
