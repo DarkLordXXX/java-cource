503a214d15bccf4711af717d47c5de486dad1864
Your Name <rakzar041@gmail.com>
2024-07-20 22:24:46 +0300

    Тут дз 6 уроку 2 завдання Обчислення площі КРУГА.

--- Lesson6/Circle.java
+++ Lesson6/Circle.java
@@ -1,11 +1,11 @@
 package Lesson6;
 
-public class Сircle {
+public class Circle {
     public static void main(String[] args) {
         double P = 3.14;
         int radius = 10;
         int r2 = radius * radius;
-        double areaсircle = P * r2;
-        System.out.println(areaсircle);
+        double area = P * r2;
+        System.out.println(area);
     }
 }
