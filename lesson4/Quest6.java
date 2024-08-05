839aa426b5757884b463cd4786e030283a2fdc26
Your Name <rakzar041@gmail.com>
2024-08-05 21:56:08 +0300

    Тут створив клас  Quest3, для 4 уроку виконня 6 завдання. Знаходження середнього значення масиву.

--- lesson4/Quest6.java
+++ lesson4/Quest6.java
@@ -4,8 +4,8 @@ public class Quest6 {
     public static void main(String[] args) {
 
         int[] values = {3, 6, 9, 12};
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
+        int max = 10;
+        int min = 4;
         int sum = 1;
 
         for (int d : values) {
@@ -18,6 +18,7 @@ public class Quest6 {
             }
             System.out.println(max);
             System.out.println(min);
+            System.out.println(sum);
         }
     }
 }
