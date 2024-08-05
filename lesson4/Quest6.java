37fdfaebf65986821295f0bc53e769611b35ab71
Your Name <rakzar041@gmail.com>
2024-08-05 22:18:58 +0300

    4.6 перепоправив 3 раз

--- lesson4/Quest6.java
+++ lesson4/Quest6.java
@@ -2,24 +2,23 @@ package lesson4;
 
 public class Quest6 {
     public static void main(String[] args) {
-
         int[] values = {3, 6, 9, 12};
-        int max = 10;
-        int min = 4;
-        int sum = 1;
+        int max = 0;
+        int min = 1;
+        int sum = 0;
 
         for (int d : values) {
-            sum +=d;
             if (d < min) {
-                min += d;
+                min = d;
             }
             if (d > max) {
                 max = d;
             }
-            System.out.println(max);
-            System.out.println(min);
-            System.out.println(sum);
         }
+            System.out.println("Max : + " +max);
+            System.out.println("Min : + " + min);
+            System.out.println("Sum : + " + sum);
     }
 }
+
 
