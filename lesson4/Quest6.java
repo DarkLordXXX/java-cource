dcc2ef04b4818eb17dbc1eb64bec8bbc71f3499c
Your Name <rakzar041@gmail.com>
2024-08-15 22:10:41 +0300

    4.6 Шосте завдання  завдання 4 уроку

--- lesson4/Quest6.java
+++ lesson4/Quest6.java
@@ -1,31 +1,39 @@
 package lesson4;
 
+import java.util.Scanner;
+
 public class Quest6 {
     public static void main(String[] args) {
-        int[] numbers = {5, 18, 3, 25, 7, 10};;
+        System.out.println("Enter enum : ");
+        Scanner scanner = new Scanner(System.in);
+        int user_num = scanner.nextInt();
 
-        int max = numbers[0];
-        int min = numbers[0];
-        int sum = 0;
-
-        for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] > max) {
-                max = numbers[i];
-            }
-            if (numbers[i] < min) {
-                min = numbers[i];
-                for (int value : numbers) {
-                    sum += value;
-                }
-            }
+        short [] nums = new short[user_num];
+        for(int i = 0;i < user_num; i++){
+            System.out.println("Enter element " + (i + 1) + " :" );
+            nums[i] = scanner.nextShort();
         }
-        System.out.println("Max : " + max);
-        System.out.println("Min : " + min);
-        System.out.println("Sum : " + sum);
-        System.out.println(numbers.length);
+
+        short max_num = nums[0];
+        for(int i = 0;i < user_num; i++) {
+            if(nums[i] > max_num)
+                max_num = nums[i];
+        }
+        System.out.println("Max element :" + max_num);
+
+        short min_num = nums[0];
+        for(int i = 0;i > user_num; i++) {
+            if(nums[i] < min_num)
+                min_num = nums[i];
+        }
+        System.out.println("Min element :" + min_num);
+
+        for(int i = 0;i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
     }
 }
-    //6) Напишіть програму на Java для знаходження максимального та мінімального
-    //значення масиву.
+
+//6) Напишіть програму на Java для знаходження максимального та мінімального значення масиву.
 
 
